9999c390e131bae4806055fd569596284fdb120c
LAPTOP-DEH7JUPD\Pham Hieu <HieuPT73@fpt.com>
2025-06-06 07:37:40 +0700

    commit

--- src/main/reducers.ts
+++ src/main/reducers.ts
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import employeeReducer from "../main/employee/reducers.ts";
 import userReducer from "../main/login/reducers.ts";
 import productReducer from "../main/product/reducers.ts";
 
 export const store = configureStore({
   reducer: {
-    employee: employeeReducer,
     user: userReducer,
     product: productReducer,
   },

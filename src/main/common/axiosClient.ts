54beaa18d203c355c7908916226aaa567c63b371
Tên GitHub <hieupham10903@gmail.com>
2025-06-10 20:02:38 +0700

    commit fix bug

--- src/main/common/axiosClient.ts
+++ src/main/common/axiosClient.ts
@@ -5,6 +5,7 @@ const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || "/",
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
